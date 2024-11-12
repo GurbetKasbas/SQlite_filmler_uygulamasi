84f79375cf0b8e7c56d865005d768480d91c3f49
GurbetKasbas <gurbetkasbas@gmail.com>
2024-11-12 21:59:39 +0300

    Anasayfa arayüzünü ekle

--- lib/screens/anasayfa.dart
+++ lib/screens/anasayfa.dart
@@ -1,4 +1,7 @@
+import 'package:film_uygulamasi/screens/filmlerSayfa.dart';
 import 'package:flutter/material.dart';
+
+import '../Kategoriler.dart';
 
 class Anasayfa extends StatefulWidget {
   const Anasayfa({super.key});
@@ -8,20 +11,68 @@ class Anasayfa extends StatefulWidget {
 }
 
 class _AnasayfaState extends State<Anasayfa> {
+
+//Kategoriler sınıfından nesne veren bir liste verir bu method
+Future<List<Kategoriler>> tumKategorileriGoster() async{
+  var kategoriListesi = <Kategoriler>[];
+
+  var k1 = Kategoriler(1, "Komedi");
+   var k2 = Kategoriler(2, "Bilim Kurgu");
+
+   kategoriListesi.add(k1);
+   kategoriListesi.add(k2);
+
+   return kategoriListesi;
+}
+
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.deepOrangeAccent,
         title: Text("Kategoriler"),
       ),
-      body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
+      body: FutureBuilder <List<Kategoriler>>(
+        future: tumKategorileriGoster(), 
+        builder: (context,snapshot){
+          //Bu future yapısı bize içerinde kategoriler nesnesi olan bir liste döndürür.
+          //Bu liste dolu mu boş mu kontrolu 
+
+          if(snapshot.hasData){
+            var kategoriListesi = snapshot.data;
+            return ListView.builder(
+              itemCount: kategoriListesi!.length,
+              itemBuilder: (context ,index){
+                var kategori = kategoriListesi[index];
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=> FilmlerSayfa()));
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.only(top:18.0),
+                    child: Card(
+                      color: Colors.indigo,
+                      child: SizedBox(
+                        height: 50,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(kategori.kategori_ad, style: TextStyle(color:Colors.white),),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              }
               
-            ],
-          ),
-        )
+              );
+          }else{
+            return Text("Veri Alınamadı.");
+          }
+        }
+        ),
     );
   }
 }
