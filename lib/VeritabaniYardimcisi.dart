46d18eeadf3e7ac3c9357ef908523a09d522e5c0
GurbetKasbas <gurbetkasbas@gmail.com>
2024-11-14 20:41:58 +0300

    Veritabanı erişimi için sınıf ekle

---
+++ lib/VeritabaniYardimcisi.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sqlite_api.dart';
+
+class VeritabaniYardimcisi{
+
+  static final String veritabaniAdi = "filmler.sqlite";
+
+  static Future<Database> veritabaniErisim () async{
+    String veritabaniYolu = join (await getDatabasesPath(),veritabaniAdi);
+
+//Veritabani var mı yok mu kontol 
+    if(await databaseExists(veritabaniYolu)){
+      print("Veritabani zaten var.Kopyalamaya gerek yok");
+    }else{
+      //asetten veritabanının alınması
+      ByteData data = await rootBundle.load("veritabani/$veritabaniAdi");
+      //Veritabanın kopyalanması için byte dönüşümü
+      List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+      //Veritabanın kopyalanması
+      await File(veritabaniYolu).writeAsBytes(bytes,flush: true);
+      print("Veri tabanı kopyalandı");
+    }
+
+  //Veritabanını açıyoruz
+    return openDatabase(veritabaniYolu);
+
+
+  }
+
+
+
+
+
+}
